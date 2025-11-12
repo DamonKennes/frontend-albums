f3b7518eff5f6fa3540df10ef792cc321cfb4385
Damon <damonkennes@gmail.com>
2025-11-12 09:26:45 +0100

    code cleanup -> delete albums route

--- app/routes/login.js
+++ app/routes/login.js
@@ -5,7 +5,7 @@ import { inject as service } from '@ember/service';
 export default class LoginRoute extends Route {
   @service session;
 
-  beforeModel(transition) {
+  beforeModel() {
     this.session.prohibitAuthentication('index');
   }
 }
--- app/routes/register.js
+++ app/routes/register.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 export default class RegisterRoute extends Route {
   @service session;
 
-  beforeModel(transition) {
+  beforeModel() {
     this.session.prohibitAuthentication('index');
   }
 }
